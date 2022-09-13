b9d3498d690cd041ff771257a4ee722ef2b55c1b
dorjoo <dorjoohover@gmail.com>
2022-09-13 16:48:53 +0800

    asdf

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,12 @@
 import Footer from "./footer";
 import Navbar from "./Navbar";
 
-export default function Layout({children}) {
-    return (
-        <>
-        <Navbar/>
-            <main>{children}</main>
-            <Footer/>
-        </>
-    )
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
